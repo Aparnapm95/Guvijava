3a9eab58bb3a84e96d9e96bc6c6c39db1c955678
Aparnapm95 <aparnaskzr1995@gmail.com>
2025-01-08 22:40:27 +0530

    Add files via upload

---
+++ Lengthofstring.java
@@ -0,0 +1,11 @@
+package guviprg;
+
+public class Lengthofstring {
+
+	public static void main(String[] args) {
+	String msg ="GUVI GEEK"	;
+			System.out.println(msg.length());// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Swaping.java
@@ -0,0 +1,19 @@
+package guviprg;
+
+public class Swaping {
+
+	public static void main(String[] args) {
+		int x = 22, y = 33;
+	System.out.println("Numbers before Swaping");
+	System.out.println("x=" +x);
+	System.out.println("y=" +y);
+	int temp = x;
+	x=y;
+	y = temp;
+	System.out.println("Numbers aftre  Swaping");
+	System.out.println("x=" +x);
+	System.out.println("y=" +y);
+
+	}
+
+}
